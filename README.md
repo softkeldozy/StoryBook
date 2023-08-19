56578a62a18f539e11fca72ffcadebed5c81f554
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-08-19 07:41:10 +0100

    Added a readme file and cleaned up app,js file

---
+++ README.md
@@ -0,0 +1,22 @@
+# StoryBooks
+
+> Create public and private stories from your life
+
+This app uses Node.js/Express/MongoDB with Google OAuth for authentication
+
+## Usage
+
+Create a config.env file in the config directory and add your mongoDB URI and Google OAuth credentials(ID and SECRET KEY) to the config.env file.
+
+You can also create a PORT variable in the config,env file too
+
+```JavaScript
+
+# Install dependencies
+npm install
+
+# Run in development
+npm run dev
+
+# Run in production
+npm start
--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const mongoose = require('mongoose');
-// const morgan = require('morgan');
-// const exphbs = require('express-handlebars');
-// const path = require('path');
-// const passport = require('passport');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo'); //must be below session
-// const connectDB = require('./config/db');
-
 const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
